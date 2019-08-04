36b7eeb858181b66ec46be37e85e06d4c7bf3539
Willians Briceño <williambriceo567@gmail.com>
2019-08-04 09:43:44 -0400

    CLI module refactored.

--- src/cli.py
+++ src/cli.py
@@ -2,14 +2,13 @@ import argparse
 
 from src.counter import regular_count
 
-def run():
-    parser = argparse.ArgumentParser(description="Word Frequency Analyzer")
-    parser.add_argument('-f', dest='filename', help="Path to the text file for the analysis.")
-    parser.add_argument('-w', action="append", dest='wordlist', default=[], help="Add a word to analyze.")
+def process_words(args, file):
+    for word in args.wordlist:
+        print("Analyzing " + word + " ... ", end='')
+        count = regular_count(word, file)
+        print(count)
 
-    args = parser.parse_args()
-    print(args.filename)
-
+def analysis(args):
     try:
         fileTmp = open(args.filename, 'r')
     except FileNotFoundError:
@@ -17,10 +16,15 @@ def run():
         exit(1)
     
     print("\nFile \"" + args.filename + "\" opened for analysis...\n")
-
-    for word in args.wordlist:
-        print("Analyzing " + word + " ... ", end='')
-        count = regular_count(word, fileTmp)
-        print(count)
-
+    process_words(args, fileTmp)
     fileTmp.close()
+
+def run():
+    parser = argparse.ArgumentParser(description="Word Frequency Analyzer")
+    parser.add_argument('-f', dest='filename', help="Path to the text file for the analysis.")
+    parser.add_argument('-w', action="append", dest='wordlist', default=[], help="Add a word to analyze.")
+    parser.add_argument('-i', action='store_true', default=False, dest='boolean_switch', 
+                        help='Switch to activate the "Continous Present" -ing inflection.')
+
+    args = parser.parse_args()
+    analysis(args)
