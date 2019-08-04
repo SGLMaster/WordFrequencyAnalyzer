5cd461d4193c6af3d29337bb54e3959344e40c88
Willians Briceño <williambriceo567@gmail.com>
2019-08-04 10:20:02 -0400

    New CLI option to set a dump file for the results.

--- src/cli.py
+++ src/cli.py
@@ -11,7 +11,8 @@ def run():
     analysis(args)
 
 def configure_switches(parser):
-    parser.add_argument('-f', dest='filename', help="Path to the text file for the analysis.")
+    parser.add_argument('-f', dest='input_filename', help="Path to the text file to analyze.")
+    parser.add_argument('-o', dest='output_filename', help="Path to the output text file to dump the results.")
     parser.add_argument('-w', action="append", dest='wordlist', default=[], help="Add a word to analyze.")
     parser.add_argument('-i', action='store_true', default=False, dest='switch_ing', 
                         help='Switch to activate the "Continous Present" -ing inflection.')
@@ -24,27 +25,36 @@ def configure_switches(parser):
 
 def analysis(args):
     try:
-        fileTmp = open(args.filename, 'r')
+        input_file = open(args.input_filename, 'r')
     except FileNotFoundError:
-        print("File not found. Please enter a valid filename.")
+        print("Input file not found. Please enter a valid filename.")
+        exit(1)
+
+    try:
+        output_file = open(args.output_filename, 'w')
+    except FileNotFoundError:
+        print("Output file not found. Please enter a valid filename.")
         exit(1)
     
-    print("\nFile \"" + args.filename + "\" opened for analysis...\n")
-    process_words(args, fileTmp)
-    fileTmp.close()
+    print("\nFile \"" + args.input_filename + "\" opened for analysis...\n")
+    process_words(args, input_file, output_file)
+    input_file.close()
+    output_file.close()
 
-def process_words(args, file):
+def process_words(args, input_file, output_file):
     for word in args.wordlist:
         print("Analyzing " + word + " ... ", end='')
-        count = counter.regular_count(word, file)
+        count = counter.regular_count(word, input_file)
 
         if args.switch_ing:
-            count += counter.regular_count(infl.inflect_ing(word), file)
+            count += counter.regular_count(infl.inflect_ing(word), input_file)
         if args.switch_plural:
-            count += counter.regular_count(infl.inflect_plural(word), file)
+            count += counter.regular_count(infl.inflect_plural(word), input_file)
         if args.switch_past:
-            count += counter.regular_count(infl.inflect_past(word), file)
+            count += counter.regular_count(infl.inflect_past(word), input_file)
         if args.switch_er:
-            count += counter.regular_count(infl.inflect_er(word), file) 
+            count += counter.regular_count(infl.inflect_er(word), input_file) 
+
+        output_file.write(word + ":" + str(count) + "\n")
 
         print(count)
