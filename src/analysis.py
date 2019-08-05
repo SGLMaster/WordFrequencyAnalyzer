cc36f5095fb8b36433aee6812bb7b21239991ed9
Willians Briceño <williambriceo567@gmail.com>
2019-08-05 16:39:21 -0400

    DRY refactoring to analysis module.

--- src/analysis.py
+++ src/analysis.py
@@ -9,30 +9,24 @@ def analyze(args, logger):
 def process_words(args, input_file, logger):
     for word in args.wordlist:
         count = counter.regular_count(word, input_file) # This is the default count without inflections
-        total_count = count
-
         logger.log(" " + word + ":" + str(count))
 
+        total_count = count
+
         if args.switch_ing:
-            word_ing = infl.inflect_ing(word)
-            count_ing = counter.regular_count(word_ing, input_file)
-            logger.log("+" + word_ing + ":" + str(count_ing))
-            total_count += count_ing
+            total_count += get_inflected_count(word, infl.inflect_ing, input_file, logger)
         if args.switch_plural:
-            word_plural = infl.inflect_plural(word)
-            count_plural = counter.regular_count(word_plural, input_file)
-            logger.log("+" + word_plural + ":" + str(count_plural))
-            total_count += count_plural
+            total_count += get_inflected_count(word, infl.inflect_plural, input_file, logger)
         if args.switch_past:
-            word_past = infl.inflect_past(word)
-            count_past = counter.regular_count(word_past, input_file)
-            logger.log("+" + word_past + ":" + str(count_past))
-            total_count += count_past
+            total_count += get_inflected_count(word, infl.inflect_past, input_file, logger)
         if args.switch_er:
-            word_er = infl.inflect_er(word)
-            count_er = counter.regular_count(word_er, input_file)
-            logger.log("+" + word_er + ":" + str(count_er))
-            total_count += count_er
+            total_count += get_inflected_count(word, infl.inflect_er, input_file, logger)
 
         logger.log("----------------------------------------------------------------")
         logger.log(" Total:" + str(total_count) + "\n")
+
+def get_inflected_count(word, inflection_func, input_file, logger):
+    inflected_word = inflection_func(word)
+    count = counter.regular_count(inflected_word, input_file)
+    logger.log("+" + inflected_word + ":" + str(count))
+    return count
