a512fcf5f7f46acdde990b8176cc62163f5add09
Willians Briceño <williambriceo567@gmail.com>
2019-08-07 10:58:53 -0400

    Now files with special formats can be analyzed

--- src/counter.py
+++ src/counter.py
@@ -1,15 +1,23 @@
 import src.inflection as infl
 
+def remove_special_chars(text):
+    special_chars = '+-*\\/"\'<>$&#@_}{][|¡!¿?:;,.='
+    for char in special_chars:
+        text = text.replace(char, ' ')
+    return text
+
 def regular_count(wordToFind, fileToAnalyze):
     count = 0
 
     fileToAnalyze.seek(0)
     textInFile = fileToAnalyze.read()
 
+    # Removing the special characters from the text allows us to analyze files with formats like .html
+    textInFile = remove_special_chars(textInFile) 
     wordsInText = textInFile.split()
 
     for word in wordsInText:
-        if(word.lower().strip('-*\\/."\' ') == wordToFind.lower()):
+        if(word.lower() == wordToFind.lower()):
             count = count + 1
 
     return count
