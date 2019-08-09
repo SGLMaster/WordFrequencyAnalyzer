ede6249305f9ac034937d844f5d647a639380b23
Willians Briceño <williambriceo567@gmail.com>
2019-08-09 15:08:18 -0400

    Refactor counter module with new functions

--- src/counter.py
+++ src/counter.py
@@ -77,25 +77,7 @@ def get_word_count(word_to_find, filenames):
     count = 0
 
     for cur_filename in filenames:
-        if cur_filename.endswith(".pdf"):
-            # PDF Miner
-            resource_manager = PDFResourceManager()
-            fake_file_handle = io.StringIO()
-            converter = TextConverter(resource_manager, fake_file_handle)
-            page_interpreter = PDFPageInterpreter(resource_manager, converter)
-
-            with open(cur_filename, 'rb') as file_to_analyze:
-                for page in PDFPage.get_pages(file_to_analyze, caching=True, check_extractable=True):
-                    page_interpreter.process_page(page)
-
-            text_in_file = fake_file_handle.getvalue()
-
-            converter.close()
-            fake_file_handle.close()
-        else:
-            with open(cur_filename, 'r', encoding="utf-8") as file_to_analyze:
-                text_in_file = file_to_analyze.read()
-
+        text_in_file = get_text_from_file(cur_filename)
         text_in_file = remove_special_chars(text_in_file)
         words_in_text = text_in_file.split()
 
@@ -104,3 +86,33 @@ def get_word_count(word_to_find, filenames):
                 count = count + 1
 
     return count
+
+def get_text_from_file(filename):
+    text_in_file = ""
+
+    if filename.endswith(".pdf"):
+        text_in_file = get_text_from_pdf(filename)
+    else:
+        with open(filename, 'r', encoding="utf-8") as file_to_analyze:
+            text_in_file = file_to_analyze.read()
+
+    return text_in_file
+
+def get_text_from_pdf(filename):
+    # PDF Miner
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    with open(filename, 'rb') as file_to_analyze:
+        for page in PDFPage.get_pages(file_to_analyze, caching=True, 
+        check_extractable=True):
+            page_interpreter.process_page(page)
+
+    text_in_file = fake_file_handle.getvalue()
+
+    converter.close()
+    fake_file_handle.close()
+
+    return text_in_file
